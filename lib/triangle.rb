4dd1001c3977432b78f0789349324790797c7d31
Samantha Barros <samantha.barros95@gmail.com>
2020-06-28 01:25:08 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -10,9 +10,9 @@ attr_accessor :side1, :side2, :side3
   end 
 
   def kind(side1,side2,side3)
-     if (side_1 <= 0) || (side_2 <= 0) || (side_3 <= 0)
+     if (side1 <= 0) || (side2 <= 0) || (side3 <= 0)
       raise TriangleError
-    elsif (side_1+side_2 <= side_3) || (side_1+side_3 <= @side_2) || (side_2+side_3 <= side_1)
+    elsif (side1+side_2 <= side_3) || (side1+side3 <= side2) || (side2+side3 <= side1)
       raise TriangleError
     else
     if (side1 == side2) && (side2 == side3)
