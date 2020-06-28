241c525fba6c3a239c6b7f1da6af3f027715a415
Samantha Barros <samantha.barros95@gmail.com>
2020-06-28 01:26:53 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,17 +9,17 @@ attr_accessor :side1, :side2, :side3
     @side3 = side3
   end 
 
-  def kind(side1,side2,side3)
-     if (side1 <= 0) || (side2 <= 0) || (side3 <= 0)
+  def kind
+     if (@side1 <= 0) || (@side2 <= 0) || (@side3 <= 0)
       raise TriangleError
-    elsif (side1+side_2 <= side_3) || (side1+side3 <= side2) || (side2+side3 <= side1)
+    elsif (@side1+@side2 <= @side_3) || (@side1+@side3 <= @side2) || (@side2+@side3 <= @side1)
       raise TriangleError
     else
-    if (side1 == side2) && (side2 == side3)
+    if (@side1 == @side2) && (@side2 == @side3)
       :equilateral
-    elsif (side1 == side2) || (side2 == side3) || (side1 == side3)
+    elsif (@side1 == @side2) || (@side2 == @side3) || (@side1 == @side3)
       :isosceles
-    elsif (side1 != side2) || (side2 != side3) || (side1 != side3)
+    elsif (@side1 != @side2) || (@side2 != @side3) || (@side1 != @side3)
       :scalene
     end 
   end 
