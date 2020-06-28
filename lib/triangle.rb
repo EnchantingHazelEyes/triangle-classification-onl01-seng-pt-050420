23a18a214c16475eb92e74f6da1edf0c21351e55
Samantha Barros <samantha.barros95@gmail.com>
2020-06-28 01:19:44 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -12,7 +12,9 @@ attr_accessor :side1, :side2, :side3
   def kind(side1,side2,side3)
     if (side1 == side2) && (side2 == side3)
       :equilateral
-    elsif (side1 == side2) 
+    elsif (side1 == side2) || (side2 == side3) || (side1 == side3)
+      :isosceles
+      el
     
     
   end 
